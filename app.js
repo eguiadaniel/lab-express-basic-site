da6bbbff4bc1a25044b24db27d08f9ab823f9a29
eguiadaniel <eguiadaniel@users.noreply.github.com>
2020-12-12 11:47:03 +0100

    bootstrap folder downloaded, next gonna try npm install bootstrap@next

--- app.js
+++ app.js
@@ -2,20 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('home');
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+app.get('/', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home.html');
 });
 
-app.get('/about', (request, response) => {
-  response.send('about');
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/views/about.html');
 });
 
-app.get('/works', (request, response) => {
-  response.send('works');
+app.get('/works', (request, response, next) => {
+  response.sendFile(__dirname + '/views/works.html');
 });
 
-app.get('/gallery', (request, response) => {
-  response.send('gallery');
+app.get('/gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/gallery.html');
+});
+
+app.get('*', (request, response, next) => {
+  response.sendFile(__dirname + '/views/home.html');
 });
 
 app.listen(3000);
